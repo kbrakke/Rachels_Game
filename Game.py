41f91d4bd524b6897c7b9f55a4707664451222aa
kbrakke <kbrakke@gmail.com>
2014-04-20 14:40:04 -0400

    Major game updates
    Lots of good stuff

--- Game.py
+++ Game.py
@@ -1,50 +1,27 @@
 import pygame, sys
 
+WIN_WIDTH = 1280
+WIN_HEIGHT = 720
+HALF_WIDTH = int(WIN_WIDTH / 2)
+HALF_HEIGHT = int(WIN_HEIGHT / 2)
+
 class PythonGame:
     def __init__(self):
         #Dictionary of potential background images
-        self.backgrounds = {}
-        self.scenes = {}
-        self.interactive = {}
-        #TODO: self.npcs{}
-        
-        pygame.init()
-        self.screen = pygame.display.set_mode((800, 700))
-        self.player = Item(pygame.image.load('persontest.png').convert(), 40, 60, 400, 540)
-        #Give the player his list of images
-        #self.player['default'] = pygame.image.load('persontest.png').convert()
-        #Initialize the background list
-        #self.backgrounds["grey_street"] = pygame.image.load('teststreet1.png').convert()
-        #self.backgrounds["red_street"] = pygame.image.load('teststreet2.png').convert()
-        #self.screen.blit(self.backgrounds["grey_street"], (0, 0))
-        self.scenes["scene1"] = Scene(pygame.image.load('teststreet1.png').convert(),
-                                      [Item(pygame.image.load('testitem1.png').convert(),
-                                            30,
-                                            60,
-                                            700,
-                                            300),
-                                       Item(pygame.image.load('testitem2.png').convert(),
-                                            40,
-                                            60,
-                                            100,
-                                            300)],
-                                      800,
-                                      600)
-        self.currentScene = "scene1"
-        self.scenes[self.currentScene].draw(self.screen)
+        self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT+100))
+        self.player = Player(pygame.image.load('persontest.png').convert())
+        self.scene = Scene(pygame.image.load('testbglong1.png'), None, None)
 
-    def _update(self, movement):
-        #player_rect = pygame.Rect(self.player_pos, self.player_height, 40, 60)
-        #self.screen.blit(self.backgrounds["grey_street"], player_rect, player_rect)
-        #self.screen.blit(self.player['default'], player_rect.move(movement, 0))
-        #self.player_pos += movement
-        self.player.move(movement, 0, self.screen, self.scenes[self.currentScene].background)
+    def update(self, movement):
+        self.player.move(movement, 0)
+        self.scene.draw(self.screen)
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.time.delay(100)
 
-    def _run(self):
+    def run(self):
+        movement = 0
         while 1:
-            movement = 0
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
@@ -54,35 +31,35 @@ class PythonGame:
                         movement = -10
                     if event.key == pygame.K_RIGHT:
                         movement = 10
-                #if event.type == pygame.KEYUP:
-                #    movement = 0
-                #else:
-                #    movement = 0
-                self._update(movement)        
-            #print('EVENT: '+str(event))
+                if event.type == pygame.KEYUP:
+                    movement = 0
+            self.update(movement)
             
 
 
-class Scene:
-    def __init__(self, background, items, width, height):
+class Scene(object):
+    def __init__(self, background, items, npcs):
         self.background = background
         self.items = items
-        self.width = width
-        self.height = height
+        self.npcs = npcs
+        self.rect = (0,0, 1280, 720)
     def draw(self, screen):
-        screen.blit(self.background, (0,0))
-        for item in self.items:
-            print("draw item")
-            item.draw(screen)
-        
-class Item:
-    def __init__(self, image, width, height, x, y):
+        screen.blit(self.background, self.rect)
+##        for item in self.items:
+##            item.draw(screen)
+##        for npc in self.npcs:
+##            pass
+            
+class Item(pygame.sprite.Sprite):
+    def __init__(self, image, width, height, x, y, name):
+        pygame.sprite.Sprite.__init__(self)
         self.image = image
         self.width = width
         self.height =  height
         self.x = x
         self.y = y
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)       
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.name = name
     def draw(self, screen):
         screen.blit(self.image, self.rect)
     def move(self, x_adj, y_adj, screen, background):
@@ -92,5 +69,22 @@ class Item:
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)  
         self.draw(screen)
         
+class Player(pygame.sprite.Sprite):
+    def __init__(self, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.inventory = {}
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+    def move(self, x_adj, y_adj):
+        x = min(self.rect.x + x_adj, WIN_WIDTH - self.rect.width)
+        x = max(0, x)
+        self.rect.x = x
+        self.rect.y = self.rect.y + y_adj
+    def pickupItem(item):
+        self.inventory[item.name] = item    
+
+        
 game = PythonGame()
-game._run()
+game.run()
--- pygameImageTest.py
+++
@@ -1,7 +0,0 @@
-import sys, pygame
-pygame.init()
-
-background = [1,1,2,2,2,1]
-screen = [0]*6
-screen[3] = 8
-playerpos = 3
